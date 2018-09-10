50b72878c100d759bf295719669e5bae7c6b8fc3
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-09-10 20:02:32 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,42 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+$k = 0;
+	rekursif1(5);
+
+	function rekursif2($l, $kolom){
+		global $k;
+		if ($l > $k - $kolom) {
+			echo "*";
+			rekursif2($l - 1, $kolom);
+		}
+	}
+
+	function rekursif1($kolom){
+		global $k;
+		rekursif2($k, $kolom);
+		echo "<br>";
+		if (--$kolom > $k) {
+			rekursif1($kolom);
+		}
+	}
+
+	echo "/*=============================================*/<br>";
+
+	$n = 5;
+	function rekursif($i, $j, $n){
+		if ($i <= $n) {
+			if ($j <= $i) {
+				echo $j;
+				$j++;
+				rekursif($i, $j, $n);
+			}
+			else{
+				echo "<br>";
+			$i++;
+			rekursif($i, 1, $n);
+			}
+		}
+	}
+
+	rekursif(1,1,$n);
 ?>
